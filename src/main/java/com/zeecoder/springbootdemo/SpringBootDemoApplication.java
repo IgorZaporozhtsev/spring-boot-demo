6e71c3381da59d65866ec7914145c3523922e15b
ihor <i.zaporozhtsev@gmail.com>
2023-06-20 14:45:30 +0300

    move n+1 select problem another branch

--- src/main/java/com/zeecoder/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/zeecoder/springbootdemo/SpringBootDemoApplication.java
@@ -1,8 +1,5 @@
 package com.zeecoder.springbootdemo;
 
-import com.zeecoder.springbootdemo.domain.Order;
-import com.zeecoder.springbootdemo.repository.ItemRepository;
-import com.zeecoder.springbootdemo.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,27 +10,12 @@ import org.springframework.context.annotation.Bean;
 @RequiredArgsConstructor
 public class SpringBootDemoApplication {
 
-    private final OrderRepository orderRepository;
-    private final ItemRepository itemRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
     @Bean
     CommandLineRunner commandLineRunner(){
-        return args -> {
-            nPlusOneSelect();
-
-        };
+        return  null;
     }
-
-    public void nPlusOneSelect() {
-        var orders = orderRepository.findAll();
-        for (Order order : orders) {
-            System.out.println(order.getItems());
-        }
-
-    }
-
 }
